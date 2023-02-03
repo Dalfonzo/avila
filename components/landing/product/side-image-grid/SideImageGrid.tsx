ae4c6a276e696fe84d82cc736c5dd3912c43a31d
Dap <dalfonzo18@gmail.com>
2023-02-03 14:19:24 -0400

    UI - Fixes aspect ratio

--- components/landing/product/side-image-grid/SideImageGrid.tsx
+++ components/landing/product/side-image-grid/SideImageGrid.tsx
@@ -13,13 +13,7 @@ const SideImageGrid = ({
   <div className={`flex gap-10 ${reversed ? 'flex-row-reverse' : 'flex-row'}`}>
     <div className="relative w-fit">
       <div className="bg-[#413c3820] absolute top-0 left-0 w-full h-full" />
-      <Image
-        src={image}
-        alt="Imagen de publicidad"
-        className="aspect-[31/50]"
-        width={440}
-        height={710}
-      />
+      <Image src={image} alt="Imagen de publicidad" width={440} height={710} />
     </div>
     <div className="grid grid-cols-3 grid-rows-2 gap-y-2 flex-1">
       {children}
